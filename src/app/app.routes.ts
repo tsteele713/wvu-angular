2a34095c07dffef0d07190b3fb17837aa154dc5c
tsteele713 <steele.tyler13@gmail.com>
2017-08-30 07:57:06 -0400

    removing all but one route as an example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { RouterModule, Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
-  { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'hero/:id',      component: HeroDetailComponent },
+  { path: 'crisis-center', component: CrisisListComponent }
 ];
